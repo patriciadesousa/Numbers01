b65fed36414580172b5b7ab43b8b45cef4642b49
unknown <git config --global user.email>
2022-12-14 22:30:58 +0000

    Passo 8

---
+++ src/main/java/com/aor/numbers/DivisibleByFilter.java
@@ -0,0 +1,18 @@
+package com.aor.numbers;
+
+public class DivisibleByFilter implements GenericListFilter {
+
+    private int divisor;
+
+    public DivisibleByFilter(int x){
+        this.divisor = x;
+    }
+
+    @Override
+    public boolean accept(Integer number){
+        if(number % divisor == 0 ){
+            return true;
+        }
+        return false;
+    }
+}
