2538e408fefa3b303f945f53f1338980a530bdd2
unknown <git config --global user.email>
2022-12-14 22:22:04 +0000

    Passo 6

--- src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -3,8 +3,9 @@ package com.aor.numbers;
 import com.aor.numbers.ListAggregator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
 import java.util.Arrays;
 import java.util.List;
 
@@ -58,14 +59,17 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        class StubListDeduplicator implements GenericListDeduplicator{
+        /*class StubListDeduplicator implements GenericListDeduplicator{
             @Override public List<Integer> deduplicate(List<Integer> list) {
                 return  Arrays.asList(1, 2, 4, 5);
             }
-        }
+        }*/
         ListAggregator aggregator = new ListAggregator();
-        StubListDeduplicator deduplicator = new StubListDeduplicator();
+        //StubListDeduplicator deduplicator = new StubListDeduplicator();
+        GenericListDeduplicator deduplicator =  Mockito.mock(GenericListDeduplicator.class);
+        Mockito.when(deduplicator.deduplicate(Mockito.anyList())).thenReturn(Arrays.asList(1, 2, 4));
+
         int distinct = aggregator.distinct(list, deduplicator);
-        Assertions.assertEquals(4, distinct);
+        Assertions.assertEquals(3, distinct);
     }
 }
